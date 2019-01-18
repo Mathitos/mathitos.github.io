b2feebf865e6c8a91533ea133f8329b80345d27a
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-01-18 16:11:07 -0200

    make webpack server work with current version of babel(add a bunch of dependencies

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const webpack = require("webpack");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.bundle.js"
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./build",
+    port: 3000,
+    watchContentBase: true
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }]
+  }
+};
