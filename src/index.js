47dd069e2d61d0836999ba8bb8fe047bebfacb94
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-01-21 02:34:08 -0200

    add component example

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import './styles/appStyles.scss';
 import Button from '@material-ui/core/Button';
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <h1>React App Running!</h1>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
     </div>
   );
 };
