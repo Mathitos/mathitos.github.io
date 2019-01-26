858ab00fb25cbf16886d3423d9b18b05f6464d3a
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-01-26 17:57:09 -0200

    add normalize css package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './views/App';
 
+import 'normalize.css'
+
 render((
   <BrowserRouter>
     <App />
