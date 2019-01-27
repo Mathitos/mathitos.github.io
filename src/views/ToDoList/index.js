d6bfc6b5f4cde98ee624508caa2aa1e8ff06595c
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-01-26 23:04:55 -0200

    add state and constructor to to-do-list widget

--- src/views/ToDoList/index.js
+++ src/views/ToDoList/index.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 import ToDoListMain from './component'
 
 class ToDoList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {text: '', key: ''}
+    }
+  }
+
   addItem = () => {
     console.log('Hello Add Item')
   }
+
   render() {
     return (
       <ToDoListMain addItem={this.addItem}/>
