ba38db6368fb2b8eca9c914bd0263ae0f4363b28
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-02-28 16:47:40 -0300

    create a auto resizeble edit text component

--- src/views/Resume/index.js
+++ src/views/Resume/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 
+import EditTextAutoResizable from '../../components/EditTextAutoResizable'
 
+const initialFontSize = 60
 export default class Resume extends Component {
-	render(){
-		return(
-			<div>
-				To Do Resume
-			</div>
-		)
-	}
+  render() {
+    return <EditTextAutoResizable />
+  }
 }
