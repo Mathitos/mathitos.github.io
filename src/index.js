999afbccf58c43e11b6107a02330e4bf69b8c1a3
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-03-13 22:50:07 -0300

    fix old path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { App } from './views/App'
+import { App } from 'scenes/App'
 
 import 'normalize.css'
 
