5b0fc041c6eed9f3560bd55496462cf564fb31a2
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-03-15 01:21:41 -0300

    bunch of style stuff to header

--- src/scenes/Header/index.js
+++ src/scenes/Header/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import HeaderMenu from './components/HeaderMenu'
 import { NavLink } from 'react-router-dom'
 
 import './header.sass'
@@ -13,8 +12,24 @@ const Header = () => (
 
 const LogoComponent = () => (
   <NavLink to="/" className="header__menu__logo">
-    {/* <img src="https://github.com/mathitos.png" /> */}
+    <img className="header__menu__logo__image" src="https://scontent.fpoa8-1.fna.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/13332752_1202231293120533_4165613431556447213_n.jpg?_nc_cat=109&_nc_ht=scontent.fpoa8-1.fna&oh=8be498a7fbcab26d86c6834cea38ccb3&oe=5D201509" />
+    {/* https://github.com/mathitos.png */}
   </NavLink>
 )
+
+const HeaderMenu = () => (
+  <div className="header__menu  flex flex-row flex-justify-end flex-align-center">
+    <MenuItem name="Resume" url="/resume" />
+    <MenuItem name="Rpg Sheets" url="/rpg-sheets" />
+    <MenuItem name="To Do List" url="/to-do-list" />
+  </div>
+)
+
+const MenuItem = ({ name, url }) => (
+  <div className="header__menu__item flex flex-justify-center flex-align-center">
+    <NavLink to={url} className="header__menu__item__content" activeClassName="header__menu__item__content--selected">{name}</NavLink>
+  </div>
+)
+
 
 export default Header
